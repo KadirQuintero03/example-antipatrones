4190626a39b3c95820f16758c9c955d0baa07047
david123456858 <juandavidperaltafuentes@gmail.com>
2025-09-09 15:59:37 -0500

    falta pruebas

--- python-clean/app/presentation/controller/weather.py
+++ python-clean/app/presentation/controller/weather.py
@@ -1,3 +1,19 @@
+from fastapi import UploadFile
+
+from app.domain.dtos.weather_data import WeatherUploadDTO
+from app.usecases.process_weather import ProcessWeatherDataUseCase
+
 class WeatherController:
-    def __init__(self,service) -> None:
+    def __init__(self,service:ProcessWeatherDataUseCase) -> None:
+        self._sevice = service
         pass
+    
+    async def upload_weather_data(self,file:UploadFile):
+        upload_instancie = WeatherUploadDTO(
+            filename= file.filename, # pyright: ignore[reportArgumentType]
+            content= await file.read()
+        )
+        
+        await self._sevice.execute(upload_instancie)
+        
+        return {"message": "Se ha guardado correctamente"}
--- python-clean/app/presentation/routes/routes.py
+++ python-clean/app/presentation/routes/routes.py
@@ -1,12 +1,21 @@
 from fastapi import APIRouter,UploadFile,File
 
+from app.usecases.process_weather import ProcessWeatherDataUseCase
+from app.presentation.controller.weather import WeatherController
+from app.insfractuctura.factories.parser_factory import factory_parser
+
 router = APIRouter(prefix='/file',tags=['files'])
 
 def moduleRouter():
     
+    factory_parser_ = factory_parser()
+    service = ProcessWeatherDataUseCase(factory_parser_)
+    controller = WeatherController(service)    
+    
     router.post('/')
     async def upload_csv(file:UploadFile = File(...)):
-        return
+        return await controller.upload_weather_data(file=file)
+        
     
     router.get('/historical')
     def get_historical():
